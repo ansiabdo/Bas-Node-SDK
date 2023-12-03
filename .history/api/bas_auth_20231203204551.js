e830d91abab98a23c9748e128edf45d41f34ff79
ansiabdo <ansi.abdooh@gmail.com>
2023-12-03 21:05:07 +0300

    userInfo added

--- .history/api/bas_auth_20231203204551.js
+++ .history/api/bas_auth_20231203204551.js
@@ -11,7 +11,6 @@ const router = express.Router();
 const CLIENTID = process.env.BAS_CLIENT_ID ?? "fbbc6c5d-c471-42dd-a46a-9a2bad1c99cd";
 const CLIENT_SECRET = process.env.BAS_CLIENT_SECERT
 const BASURL = process.env.BAS_BASE_URL
-let access_token;
 
 router.post('/userinfo', async (req, res) => {
     // Your logic to handle payment initiation
@@ -19,23 +18,14 @@ router.post('/userinfo', async (req, res) => {
     console.log("userinfo req :", req.body)
 
     if (authid) {
-        await getBasToken(authid).then(async (response) => {
+        await getToken(authid).then(async (response) => {
             let data = await response.json()
             console.log("response :", data)
-            access_token = data.access_token
-            await getBasUserInfo(access_token).then(async (user) => {
-                let userData = await user.json()
-                console.log("user :", data)
-                return res.status(200).json(userData)
-            }).catch((error) => {
-                let data = error?.response?.data ?? '{}'
-                console.error("Error :", data)
-                return res.status(409).send(data)
-            })
+            res.status(200).json(data)
         }).catch((error) => {
             let data = error?.response?.data ?? '{}'
             console.error("Error :", data)
-            return res.status(409).send(data)
+            res.status(409).send(data)
         })
     } else {
         res.status(409).json({ status: 0, success: false, msg: "Authid Required" })
@@ -44,12 +34,12 @@ router.post('/userinfo', async (req, res) => {
 
 });
 
-async function getBasToken(authid) {
-    console.log("getBasToken :", authid)
+async function getToken(authid) {
+    console.log("getToken :", authid)
     if (authid) {
         var myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-        console.log("getBasToken :", myHeaders)
+        console.log("getToken :", myHeaders)
 
         var urlencoded = new URLSearchParams();
         urlencoded.append("client_id", CLIENTID);
@@ -68,27 +58,6 @@ async function getBasToken(authid) {
         var url = `${BASURL}/api/v1/auth/token`
 
         console.log("params :", url, urlencoded.toString());
-        return await fetch(url, requestOptions)
-    }
-}
-async function getBasUserInfo(token) {
-    console.log("getBasUserInfo :", token)
-    if (token) {
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        myHeaders.append("Authorization", `Bearer ${token}`);
-        console.log("getBasUserInfo :", myHeaders)
-
-
-        var requestOptions = {
-            method: 'GET',
-            headers: myHeaders,
-            redirect: 'follow'
-        };
-
-        var url = `${BASURL}/api/v1/auth/userinfo`
-
-        console.log("params :", url);
         return await fetch(url, requestOptions)
     }
 }
---
+++ .history/config/config_20231203205146.js
@@ -0,0 +1,32 @@
+// 'use strict';
+
+// var BAS_ENVIORMENT = 'TEST';   // PROD FOR PRODUCTION
+// var BAS_FINAL_URL = '';
+var BAS_CLIENT_ID = 'fbbc6c5d-c471-42dd-a46a-9a2bad1c99cd'
+
+// const BAS_CLIENT_ID = "fbbc6c5d-c471-42dd-a46a-9a2bad1c99cd"
+// var BAS_BASE_URL_PROD = "https://stagebas.yk-bank.com:9101"
+// var BAS_BASE_URL_TEST = "https://api-tst.basgate.com:4951"
+// var BAS_APP_ID = "4bcb8478-cdf0-488d-ad42-00f7ffdc3d88"
+// var BAS_MKEY = "R0Biem8wOUIySkJxNGd6cQ=="
+// var BAS_CLIENT_SECERT = "42488bad-87f9-4b6d-9705-7e396c4f61e2"
+
+var BASE_URL = "https://bas-node-sdk.onrender.com"
+var CALLBACK_URL = "https://bas-node-sdk.onrender.com/api/v1/callback"
+
+// if (BAS_ENVIORMENT == 'TEST') {
+//   BAS_FINAL_URL = BAS_BASE_URL_TEST;
+// } else {
+//   BAS_FINAL_URL = BAS_BASE_URL_PROD;
+// }
+
+// module.exports = {
+
+//   BAS_MKEY,
+//   BAS_CLIENT_ID:BAS_CLIENT_ID,
+//   BAS_APP_ID,
+//   BAS_CLIENT_SECERT,
+//   BASE_URL,
+//   CALLBACK_URL,
+//   BAS_FINAL_URL
+// };
