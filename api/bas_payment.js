cec2911137c36f3e1fe37d9b005106a6817bc9ad
ansiabdo <ansi.abdooh@gmail.com>
2023-12-04 18:04:18 +0300

    change to 265

--- api/bas_payment.js
+++ api/bas_payment.js
@@ -39,8 +39,129 @@ router.post('/checkout', async (req, res) => {
 
 
 });
+router.get('/status/:orderId', async (req, res) => {
+    var { orderId } = req.params
+    console.log("status req.params :", req.params)
+
+    if (orderId) {
+        await paymentStatus(orderId).then(async (response) => {
+            let data = await response.json()
+            console.log("response :", data)
+            return res.status(200).json(data)
+        }).catch((error) => {
+            let data = error?.response?.data ?? '{}'
+            console.error("Error :", data)
+            return res.status(409).send(data)
+        })
+    } else {
+        res.status(409).json({ status: 0, success: false, msg: "BAS_GATE Not Seleceted" })
+    }
+
+
+});
 
 async function initPayment(order) {
+    console.log("initPayment order:", order)
+    if (order) {
+        var myHeaders = new Headers();
+        const requestTimestamp = Date.now()
+        myHeaders.append("Content-Type", "application/json");
+
+        const orderId = generateOrderId();
+        var head = {}
+        var body = {
+            "appId": APPID,
+            "requestTimestamp": requestTimestamp.toString(),
+            "orderType": "PayBill",
+            "callBackUrl": CALLBACKURL + `/${orderId}`,
+            "customerInfo": {
+                id: order.customerInfo.open_id,
+                name: order.customerInfo.name
+            },
+            "amount": {
+                "value": "1100",
+                "currency": "YER"
+            },
+            // "orderId": orderId,
+            "OrderId": "3a9b385b-c30b-4cd3-9b0e-e5887eda47b0",
+            orderDetails: {
+                "Id": "3a9b385b-c30b-4cd3-9b0e-e5887eda47b0",
+                "Products": [
+                    {
+                        "Product": "APPLE GIFT CARD $10",
+                        "Type": "Code",
+                        "Price": 5400.0,
+                        "Qty": 1,
+                        "SubTotalPrice": 5400.0
+                    },
+                    {
+                        "Product": "PUBG 60 UC",
+                        "Type": "Code",
+                        "Price": 660.0,
+                        "Qty": 9,
+                        "SubTotalPrice": 5940.0
+                    },
+                    {
+                        "Product": "APPLE GIFT CARD $40",
+                        "Type": "Code",
+                        "Price": 23400.0,
+                        "Qty": 1,
+                        "SubTotalPrice": 23400.0
+                    },
+                    {
+                        "Product": "PUBG 660 UC",
+                        "Type": "Code",
+                        "Price": 6120.0,
+                        "Qty": 2,
+                        "SubTotalPrice": 12240.0
+                    }
+                ],
+                "Currency": "YER",
+                "TotalPrice": 46980.0
+            } //order.orderDetails
+        }
+
+        let sign
+        try {
+            console.log("MKEY :", MKEY);
+            //atob("R0Biem8wOUIySkJxNGd6cQ==")
+            sign = await BasChecksum.generateSignature(JSON.stringify(body), atob(MKEY));
+            // await paytmChecksum.then(function (result) {
+            console.log("generateSignature Returns: " + sign);
+            //     sign = result
+            //     var verifyChecksum = BasChecksum.verifySignature(JSON.stringify(body), atob(MKEY), result);
+            //     console.log("verifySignature Returns: " + verifyChecksum);
+            // }).catch(function (error) {
+            //     console.log(error);
+            // });
+            // sign = await genchecksumbystring(JSON.stringify(params), MKEY)
+        } catch (error) {
+            console.log("Error :", error);
+        }
+        console.log("signature :", sign);
+        head["signature"] = sign;
+        head["requestTimestamp"] = requestTimestamp;
+
+        console.log("initPayment myHeaders:", myHeaders)
+        var params = {
+            head, body
+        }
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: JSON.stringify(params),
+            redirect: 'follow'
+        };
+
+        var url = `${BASURL}/api/v1/merchant/secure/transaction/initiate`
+
+        console.log("url :", url);
+        console.log("params :", JSON.stringify(params));
+        return await fetch(url, requestOptions)
+    }
+}
+
+async function paymentStatus(orderId) {
     console.log("initPayment order:", order)
     if (order) {
         var myHeaders = new Headers();
